d9152452989a1c0653df20fbb93e1986ed2069ea
Prechak <benedict.precha@gmail.com>
2024-05-21 15:25:44 +0700

    (ex:7): check palindrome

--- ex-7.js
+++ ex-7.js
@@ -1,8 +1,11 @@
 function isPalindrome(string) {
   // Start coding here
+  const checkReverse = string.split("").reverse().join("");
+  console.log(checkReverse);
+  return string === checkReverse;
 }
 
 //Example case
-console.log(isPalindrome("reviver"));// true
-console.log(isPalindrome("บวบ"));// true
-console.log(isPalindrome("deliver"));// false
+console.log(isPalindrome("reviver")); // true
+console.log(isPalindrome("บวบ")); // true
+console.log(isPalindrome("deliver")); // false
