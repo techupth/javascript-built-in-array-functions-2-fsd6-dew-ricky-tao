eb4c93e4b88da0e389b8bfe3094143ad53a6f958
Prechak <benedict.precha@gmail.com>
2024-05-21 15:04:01 +0700

    (ex-6):update car collection

--- ex-6.js
+++ ex-6.js
@@ -1,7 +1,13 @@
 const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
-  // Start coding here
+  if (!carCollection.includes(carBrand)) {
+    carCollection.push(carBrand);
+    return `new car collection is : ${carCollection}`;
+  } else {
+    const position = carCollection.indexOf(carBrand) + 1;
+    return `${carBrand} has already existed in the ${position} position of car collection.`;
+  }
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
